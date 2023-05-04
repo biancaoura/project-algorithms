d5a0432a2d3f97e65d068cd8b98d8fc47aa93f4b
Bianca Oura <biancaoura@gmail.com>
2023-05-03 23:45:30 -0300

    test: add encrypt message func test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('abc', 'def')
+
+    assert encrypt_message('desserts', 4) == 'stre_ssed'
+    assert encrypt_message('stressed', 9) == 'desserts'
