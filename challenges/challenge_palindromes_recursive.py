4f03c8d5baaa8eb2b40061dcab971cf78c36ae8a
Bianca Oura <biancaoura@gmail.com>
2023-05-03 23:55:23 -0300

    feat: palindrome recursive check func

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word: str, low_index: int, high_index: int) -> bool:
+    if low_index > high_index and word:
+        return True
+
+    try:
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+    except IndexError:
+        return False
