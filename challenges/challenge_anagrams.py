a3eaa828e5924e8c8506c3b5b5be3d66f41a7e61
Bianca Oura <biancaoura@gmail.com>
2023-05-04 09:16:13 -0300

    feat: anagram checker func

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,44 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def merge_sort(string: str) -> str:
+    if len(string) <= 1:
+        return string
+
+    middle = len(string) // 2
+    left = string[:middle]
+    right = string[middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left: str, right: str) -> str:
+    result = []
+    l, r = 0, 0
+
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+
+    result += left[l:]
+    result += right[r:]
+
+    return "".join(result)
+
+
+def is_anagram(first_string: str, second_string: str):
+    first_sort = merge_sort(first_string.lower())
+    second_sort = merge_sort(second_string.lower())
+
+    if not first_sort or not second_sort:
+        return (first_sort, second_sort, False)
+
+    return (
+        first_sort,
+        second_sort,
+        first_sort == second_sort,
+    )
