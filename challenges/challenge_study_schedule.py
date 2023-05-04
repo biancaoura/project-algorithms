27bc77e508729e8154d10ac2c3e6fd1b29d1816c
Bianca Oura <biancaoura@gmail.com>
2023-05-03 23:36:09 -0300

    feat: study schedule func

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time: int):
+    count = 0
+    try:
+        for period in permanence_period:
+            if (period[0] <= target_time <= period[1]):
+                count += 1
+        return count
+    except TypeError:
+        return None
